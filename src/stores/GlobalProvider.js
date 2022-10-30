1524bf4650ce2d7512a90a3a7a02eb251d41ec40
matt384 <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>
2022-10-30 15:48:44 -0700

    broken but getting there

---
+++ src/stores/GlobalProvider.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import SnackabraStore from './Snackabra.Store';
+import { useLocalStore } from 'mobx-react';
+
+const GlobalContext = createContext({});
+
+const GlobalProvider = ({ children }) => {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    const init = async () =>{
+      await store.sbStore.init();
+      setReady(true)
+    }
+    init()
+  }, [])
+  /**
+   * INCLUDE GLOBAL STORES HERE
+   */
+  const store = {
+    sbStore: new SnackabraStore(),
+  };
+  return (
+    <>
+      {ready ?
+
+        (<GlobalContext.Provider value={store}>
+          {children}
+        </GlobalContext.Provider>)
+
+        : ''
+      }
+    </>
+  );
+
+
+};
+
+const useStateValues = () => useContext(GlobalContext);
+const state = useStateValues();
+export { GlobalProvider, state };
